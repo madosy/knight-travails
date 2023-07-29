3b5e771ca972a269e4984c719e85717d41642f61
Dasom Heo <dasomee@outlook.com>
2023-07-29 19:08:51 -0400

    Clean up variable names for readability

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import possibleMoves from "./possibleMoves.js";
 import knightMoves from "./knightMoves.js";
 
 let chessBoard = Array.from({ length: 8 }, (item) => Array(8).fill(null));
-knightMoves([3, 3], [4, 1]);
+console.log(knightMoves([1, 3], [4, 3]));
 console.log();
--- knightMoves.js
+++ knightMoves.js
@@ -1,30 +1,14 @@
 import Position from "./Position.js";
 import possibleMoves from "./possibleMoves.js";
 
-const knightMoves = function ([x1, y1], [x2, y2]) {
-  // let chessBoard = Array.from({ length: 8 }, (item) => Array(8).fill(null));
+const knightMoves = function (start, target) {
+  if (start == target) return [...start];
   let searchQueue = [];
-  searchQueue.push({ position: [x1, y1], history: [] });
-  // chessBoard[x1][y1] = new Position([x1, y1]);
-  // let nextMoves = chessBoard[x1][y1].nextMoves;
-  // function searchInside([x1, y1], previous) {
-  //   if (chessBoard[x1][y1] == null) chessBoard[x1][y1] = new Position([x1, y1]);
-  //   let currentPosition = chessBoard[x1][y1];
-  //   if (currentPosition.x == x2 && currentPosition.y == y2) {
-  //     return [currentPosition.x, currentPosition.y];
-  //   } else {
-  //     searchQueue.push(...currentPosition.nextMoves);
-  //     return;
-  //   }
-  // }
-  // while (searchQueue.length > 0) {
-  //   searchInside(searchQueue[0], "");
-  // }
-  let target = [x2, y2];
+  searchQueue.push({ position: start, history: [] });
 
   function search() {
     let current = searchQueue.shift();
-    current.history.push(`${current.position.toString()}`);
+    current.history.push(current.position);
     if (current.position[0] == target[0] && current.position[1] == target[1]) {
       searchQueue = [];
       return current.history;
@@ -40,7 +24,7 @@ const knightMoves = function ([x1, y1], [x2, y2]) {
 
   while (searchQueue.length > 0) {
     let result = search();
-    console.log(result);
+    if (searchQueue.length == 0) return result;
   }
 };
 
